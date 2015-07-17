bd2543687127caff05f927151a5bb2e9eaacd4c5
Your Name <charlotte@rabbithill.net>
2015-07-17 16:58:41 +0000

    Caffeine

---
+++ app_controller.rb
@@ -0,0 +1,25 @@
+# require 'bundler'
+Bundler.require
+
+require "./models/results.rb"
+
+class MyApp < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  get '/form' do
+    erb :form
+  end
+  
+  post '/results' do 
+    
+    @suggestion = caffine_suggestor(params[:tolerance], params[:day], params[:hours].to_f)
+    
+    erb :results
+  end
+  
+
+
+end
---
+++ models/results.rb
@@ -0,0 +1,253 @@
+def caffine_suggestor(tolerance, day, hours)
+  
+
+#LOW LONG
+  if tolerance =="low"
+    if day =="long"
+          if hours >=0 &&hours <2
+            return "Congrats you are about to dope! In order to stay productive take a Large coffee with breakfast, a Medium coffee with lunch, and to top it all off a Medium coffee with dinner." 
+          end
+      end
+  end
+  if tolerance =="low"
+    if day =="long"
+          if hours >=2 &&hours <4  
+            return "Congrats you are about to dope! In order to stay productive take a Medium coffee with breakfast, and a Small coffee with lunch."
+          end
+      end
+  end
+  if tolerance =="low"
+    if day =="long"
+          if hours >=4 &&hours <6
+            return "Congrats you are about to dope! In order to stay productive take a Small coffee with breakfast, and a Small coffee with lunch."
+          end
+      end
+  end
+  if tolerance =="low"
+    if day =="long"
+          if hours >=6 &&hours <8
+            return "Congrats you are about to dope! In order to stay productive take a Small coffee with breakfast." 
+          end
+      end
+  end
+  if tolerance =="low"
+    if day =="long"
+          if hours >=8 &&hours <24   
+            return "Congrats you listened to your mother and got a full nights rest, enjoy your well deserved productive day, caffeine free."
+          end
+      end
+  end
+  
+
+  
+  
+  
+  
+#LOW SHORT
+  if tolerance =="low"
+    if day =="short"
+          if hours >=0 &&hours <2
+            return "Congrats you are about to dope! In order to stay productive take a Large coffee with breakfast and a Medium coffee with lunch." 
+          end
+      end
+  end
+  if tolerance =="low"
+    if day =="short"
+          if hours >=2 &&hours <4  
+            return "Congrats you are about to dope! In order to stay productive take a Medium coffee with breakfast, and a Small coffee with lunch."
+          end
+      end
+  end
+  if tolerance =="low"
+    if day =="short"
+          if hours >=4 &&hours <6
+            return "Congrats you are about to dope! In order to stay productive take a Small coffee with breakfast, and a Small coffee with lunch."
+          end
+      end
+  end
+  if tolerance =="low"
+    if day =="short"
+          if hours >=6 &&hours <8
+            return "Congrats you are about to dope! In order to stay productive take a Small coffee with breakfast." 
+          end
+      end
+  end
+  if tolerance =="low"
+    if day =="short"
+          if hours >=8 &&hours <24   
+            return "Congrats you listened to your mother and got a full nights rest, enjoy your well deserved productive day, caffeine free."
+          end
+      end
+  end  
+
+  
+  
+  
+  
+  
+#MEDIUM LONG
+  if tolerance =="medium"
+    if day =="long"
+          if hours >=0 &&hours <2
+            return "Congrats you are about to dope! In order to stay productive take a Nitrous coffee with breakfast (this is a redbull mixed with a large coffee), a Large coffee with lunch, and to top it all off a Large coffee with dinner." 
+          end
+      end
+  end
+  if tolerance =="medium"
+    if day =="long"
+          if hours >=2 &&hours <4  
+            return "Congrats you are about to dope! In order to stay productive take a Large coffee with breakfast, a Large coffee with lunch, and to top it all off a Medium coffee with dinner."
+          end
+      end
+  end
+  if tolerance =="medium"
+    if day =="long"
+          if hours >=4 &&hours <6
+            return "Congrats you are about to dope! In order to stay productive take a Medium coffee with breakfast, a Medium coffee with lunch, and to top it all off a Small coffee with dinner."
+          end
+      end
+  end
+  if tolerance =="medium"
+    if day =="long"
+          if hours >=6 &&hours <8
+            return "Congrats you are about to dope! In order to stay productive take a Small coffee with breakfast, a Small coffee with lunch, and to top it all off a Small coffee with dinner." 
+          end
+      end
+  end
+  if tolerance =="medium"
+    if day =="long"
+          if hours >=8 &&hours <24   
+            return "Congrats you listened to your mother and got a full nights rest, enjoy your well deserved productive day, caffeine free."
+          end
+      end
+  end
+
+  
+  
+  
+  
+#MEDIUM SHORT
+  if tolerance =="medium"
+    if day =="short"
+          if hours >=0 &&hours <2
+            return "Congrats you are about to dope! In order to stay productive take a Nitrous coffee with breakfast (this is a redbull mixed with a large coffee), and a Large coffee with lunch." 
+          end
+      end
+  end
+  if tolerance =="medium"
+    if day =="short"
+          if hours >=2 &&hours <4  
+            return "Congrats you are about to dope! In order to stay productive take a Large coffee with breakfast, and a Medium coffee with lunch."
+          end
+      end
+  end
+  if tolerance =="medium"
+    if day =="short"
+          if hours >=4 &&hours <6
+            return "Congrats you are about to dope! In order to stay productive take a Medium coffee with breakfast, and a Medium coffee with lunch."
+          end
+      end
+  end
+  if tolerance =="medium"
+    if day =="short"
+          if hours >=6 &&hours <8
+            return "Congrats you are about to dope! In order to stay productive take a Small coffee with breakfast, and a Small coffee with lunch." 
+          end
+      end
+  end
+  if tolerance =="medium"
+    if day =="short"
+          if hours >=8 &&hours <24   
+            return "Congrats you listened to your mother and got a full nights rest, enjoy your well deserved productive day, caffeine free."
+          end
+      end
+  end
+
+
+  
+ 
+  
+#HIGH LONG
+  if tolerance =="high"
+    if day =="long"
+          if hours >=0 &&hours <2
+            return "Congrats due to your stupidity you are about to see SOUND! In order to stay productive take a Nitrous coffee with breakfast (this is a redbull mixed with a large coffee), a Nitrous coffee with lunch and to top it all of a Nitrous coffee with dinner. You will most likely die due to a caffeine overdose now, so enjoy the ride!" 
+          end
+      end
+  end
+  if tolerance =="high"
+    if day =="long"
+          if hours >=2 &&hours <4  
+            return "Congrats you are about to dope! In order to stay productive take a Large coffee with breakfast, a Large coffee with lunch, and to top it all off a Large coffee with dinner."
+          end
+      end
+  end
+  if tolerance =="high"
+    if day =="long"
+          if hours >=4 &&hours <6
+            return "Congrats you are about to dope! In order to stay productive take a Large coffee with breakfast, a Medium coffee with lunch, and to top it all off a Medium coffee with dinner."
+          end
+      end
+  end
+  if tolerance =="high"
+    if day =="long"
+          if hours >=6 &&hours <8
+            return "Congrats you are about to dope! In order to stay productive take a Medium coffee with breakfast, a Medium coffee with lunch, and to top it all off a Medium coffee with dinner."
+          end
+      end
+  end
+  if tolerance =="high"
+    if day =="long"
+          if hours >=8 &&hours <24   
+            return "Congrats you listened to your mother and got a full nights rest, enjoy your well deserved productive day, caffeine free."
+          end
+      end
+  end
+  
+  
+  
+  
+  
+  
+  
+#HIGH SHORT
+  if tolerance =="high"
+    if day =="short"
+          if hours >=0 &&hours <2
+            return "Congrats due to your stupidity you are about to see SOUND! In order to stay productive take a Nitrous coffee with breakfast (this is a redbull mixed with a large coffee), and a Nitrous coffee with lunch. You will most likely die due to a caffeine overdose now, so enjoy the ride!" 
+          end
+      end
+  end
+  if tolerance =="high"
+    if day =="short"
+          if hours >=2 &&hours <4  
+            return "Congrats you are about to dope! In order to stay productive take a Large coffee with breakfast, and a Large coffee with lunch."
+          end
+      end
+  end
+  if tolerance =="high"
+    if day =="short"
+          if hours >=4 &&hours <6
+            return "Congrats you are about to dope! In order to stay productive take a Large coffee with breakfast, and a Medium coffee with lunch."
+          end
+      end
+  end
+  if tolerance =="high"
+    if day =="short"
+          if hours >=6 &&hours <8
+            return "Congrats you are about to dope! In order to stay productive take a Medium coffee with breakfast, and a Medium coffee with lunch."
+          end
+      end
+  end
+  if tolerance =="high"
+    if day =="short"
+          if hours >=8 &&hours <24   
+            return "Congrats you listened to your mother and got a full nights rest, enjoy your well deserved productive day, caffeine free."
+          end
+      end
+  end
+  
+  
+  
+end
+
